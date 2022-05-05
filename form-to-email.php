c2bebfbb8b7847be071b0bcadd1e59747f022aa9
Chris Snyder <csnyder332@gmail.com>
2022-05-05 09:32:54 -0500

    initial

---
+++ form-to-email.php
@@ -0,0 +1,57 @@
+<?php
+  if(PHP VALIDATION) {
+    echo 'You were not validated';
+  } else {
+    echo "<script src=\"script.js\"><script>"
+  }
+?>
+
+<?php
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+               
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+?>
+
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+?>
+
+<?php
+
+  $to = "csnyder332@gmail.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
